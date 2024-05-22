ac40828a64316d97f63e17c9fc34a91b05c71cf3
maru1019 <green101927.10@gmail.com>
2024-05-22 15:32:15 +0900

    ActiveHashとバリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
 
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shopping_fee_status
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
+
+
+  validates :image, :item_name, :item_info, :item_price, presence: true
+  validates :item_category_id, :item_sales_status, :item_shopping_fee_status, :prefecture, :item_scheduled_delivery, numericality: { other_than: 1 , message: "can't be blank"}
   
 end
