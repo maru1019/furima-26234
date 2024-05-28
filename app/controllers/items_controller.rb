47e8776dd6983f81d01459cc5a5ed8e4d4b4ef36
maru1019 <green101927.10@gmail.com>
2024-05-28 18:28:26 +0900

    エラーハンドリング修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,7 +33,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
      redirect_to item_path(@item)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,6 @@ class Item < ApplicationRecord
   validates :item_price, numericality: { greater_than_or_equal_to: 300, message: 'must be greater than or equal to 300' }
   validates :item_price, numericality: { less_than_or_equal_to: 9_999_999, message: 'must be less than or equal to 9999999' }
   with_options numericality: { other_than: 0, message: "can't be blank" } do
-    validates :item_category_id, :item_sales_status_id, :item_shopping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id
+    validates :item_category.name, :item_sales_status.name, :item_shopping_fee_status, :prefecture.name, :item_scheduled_delivery.name
   end
 end
