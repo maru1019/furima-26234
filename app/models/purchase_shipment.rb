712cb28cf869fca744a753e83af9b3627c9bf9f7
maru1019 <green101927.10@gmail.com>
2024-06-03 19:58:25 +0900

    修正

--- app/models/purchase_shipment.rb
+++ app/models/purchase_shipment.rb
@@ -6,7 +6,7 @@ class PurchaseShipment
     validates :post_code, :city, :addresses, :phone_number, :user_id, :item_id, :token, :prefecture_id
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only number' }
+  validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. input only number' }
   validates :phone_number, length: { minimum: 10, message: 'is too short (minimum is 10 characters)' }
   validates :phone_number, length: { maximum: 11, message: 'is too long (maximum is 11 characters)' }
   validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
