f1a2833daf6a0699aebf91677d80c33a2f48d8ea
maru1019 <green101927.10@gmail.com>
2024-06-03 18:31:50 +0900

    テストコード修正

--- spec/factories/purchase_shipment.rb
+++ spec/factories/purchase_shipment.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_shipment do
     post_code { '123-4567' }
-    prefecture_id { 1 }
+    prefecture_id { 2 }
     city { '東京都' }
     addresses { '1-1' }
     building { '東京ハイツ' }
